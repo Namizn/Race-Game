9321decbb0296187605ae8cf56129d94772db8f4
Namizn <leleo2006@outlook.com>
2024-10-16 08:36:06 -0300

    Hora de Tentar dnv
    Scripts Criados

--- Recuperação Daniel/Assets/Scripts/IFinishable.cs
+++ Recuperação Daniel/Assets/Scripts/IFinishable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ICar : MonoBehaviour
+public class IFinishable : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
