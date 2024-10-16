9daf718536ec31a4f10b91ba889e99b2af87cef7
Namizn <leleo2006@outlook.com>
2024-10-16 08:58:48 -0300

    CarController Feito

--- Recuperação Daniel/Assets/Scripts/CarController.cs
+++ Recuperação Daniel/Assets/Scripts/CarController.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun; // Biblioteca para RPC
 
-public class CarController : MonoBehaviour
+public class CarController : MonoBehaviourPun, ICar
 {
-    // Start is called before the first frame update
+    public float speed = 10f;
+    public float turnSpeed = 100f;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (photonView.IsMine)
+        {
+            float move = Input.GetAxis("Vertical") * speed;
+            float turn = Input.GetAxis("Horizontal") * turnSpeed;
+
+            Vector3 movement = transform.forward * move * Time.deltaTime;
+            Quaternion rotation = Quaternion.Euler(0, turn * Time.deltaTime, 0);
+
+            rb.MovePosition(rb.position + movement);
+            rb.MoveRotation(rb.rotation * rotation);
+
+            photonView.RPC("SyncPosition", RpcTarget.Others, rb.position, rb.rotation);
+        }
+    }
+
+    [PunRPC]
+    void SyncPosition(Vector3 position, Quaternion rotation)
+    {
+        rb.position = position;
+        rb.rotation = rotation;
+    }
+
+    public void Accelerate(float amount)
+    {
+        // Implementação da aceleração
+    }
+
+    public void Drive(float amount)
+    {
+        // Implementação da direção
+    }
+
+    public void UpdatePosition()
+    {
+        // Implementação da atualização de posição
     }
 }
