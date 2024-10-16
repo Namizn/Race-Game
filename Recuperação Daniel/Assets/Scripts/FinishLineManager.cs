91d7fb54069fb9d8517cd6d58e5c36e9ddabdbad
Namizn <leleo2006@outlook.com>
2024-10-16 08:48:08 -0300

    Linha de Chegada
    linha de chegada ta quase pronta falta apenas colocar instanciar no front

--- Recuperação Daniel/Assets/Scripts/FinishLineManager.cs
+++ Recuperação Daniel/Assets/Scripts/FinishLineManager.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public interface IFinishable
+public class FinishLineManager : MonoBehaviourPun, IFinishable
 {
-    void OnFinishLineCrossed();
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            photonView.RPC("OnFinishLineCrossed", RpcTarget.All, other.GetComponent<PhotonView>().Owner.NickName);
+        }
+    }
+
+    [PunRPC]
+    public void OnFinishLineCrossed(string playerName)
+    {
+        Debug.Log($"{playerName} cruzou a linha de chegada!");
+        // Implementação adicional quando um jogador termina a corrida
+    }
 }
