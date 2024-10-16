f6c19b56e47bca4998059dcf6bbd0007433e8af6
Namizn <leleo2006@outlook.com>
2024-10-16 08:54:56 -0300

    Interface ICAR

--- Recuperação Daniel/Assets/Scripts/ICar.cs
+++ Recuperação Daniel/Assets/Scripts/ICar.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ICar : MonoBehaviour
+public interface ICar
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Accelerate(float amount);
+    void Drive(float amount);
+    void UpdatePosition();
 }
